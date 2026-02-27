acaef543f8fbb19388a04c7a762e4312f728339a
Kushal Dubey <kushaldubey121@gmail.com>
2026-02-28 02:43:32 +0530

    Create better.cpp

---
+++ Two_pointer/Tripletwithsum0/better.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+    
+
+        vector<vector<int>>result;
+        int n=nums.size()-1;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1; i++) {
+            if( i>0 && nums[i]==nums[i-1]) {
+                continue;
+            }
+            int left = i+1;
+            int right = n;
+
+            while(left<right) {
+                int sum = nums[left]+nums[right];
+              if(sum == -1*nums[i]) {
+                result.push_back({nums[i],nums[left],nums[right]});
+                left++;
+                right--;
+
+                while(left<n && nums[left]==nums[left-1]) {
+                    left++;
+                }
+
+                while(left<right && nums[left]==nums[right+1]) {
+                    right--;
+                }
+              }
+
+              else if(sum< -1*nums[i]) {
+                left++;
+              }
+
+              else {
+                 right--;
+              }
+            }
+        }
+
+        return result;
+    }
+};
